2f199f249dba835e92eb2b209f872f8e8dbb3657
VISHNUMANIKANDAN T <155707903+VishnuMK2006@users.noreply.github.com>
2025-03-05 07:10:49 +0530

    Daily_challenge (05.03.2025)

---
+++ 05.03.2025.java
@@ -0,0 +1,21 @@
+
+/* Brute Froce */
+
+class Solution {
+    public long coloredCells(int n) {
+        if (n == 1)
+            return 1;
+        return (4 * (n - 1)) + coloredCells(n - 1);
+    }
+}
+
+/* using Equation */
+class Solution {
+    public long coloredCells(long n) {
+        return (4 * ((n * (n - 1)) / 2) + 1);
+    }
+}
+
+/*
+ * 4 * n(n-1)/2 +1
+ */
