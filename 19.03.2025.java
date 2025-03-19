f9163ad806fe83b444cb17df034147c7347b946d
VISHNUMANIKANDAN T <155707903+VishnuMK2006@users.noreply.github.com>
2025-03-19 20:14:03 +0530

    19.03.2025

---
+++ 19.03.2025.java
@@ -0,0 +1,21 @@
+
+//100% ,ms
+
+class Solution {
+    public int minOperations(int[] nums) {
+        int c=0;
+        for(int i=0;i<nums.length-2;i++){
+            if(nums[i]==0){
+                nums[i]=nums[i]^1;
+                nums[i+1]=nums[i+1]^1;
+                nums[i+2]=nums[i+2]^1;
+                c++;
+            }
+            }
+       if(nums[nums.length-1]==0 || nums[nums.length-2]==0)
+        return -1;
+        return c;
+    }
+}
+
+
