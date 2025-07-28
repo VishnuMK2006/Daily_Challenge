fa9b099ebd17fa2a847dc9529488f059c95c713d
VISHNUMANIKANDAN T <155707903+VishnuMK2006@users.noreply.github.com>
2025-07-28 19:41:06 +0530

    Time: 2 ms (90.56%) | Memory: 45.4 MB (60.84%) - LeetSync

--- 1496-lucky-numbers-in-a-matrix/lucky-numbers-in-a-matrix.java
+++ 1496-lucky-numbers-in-a-matrix/lucky-numbers-in-a-matrix.java
@@ -1,31 +1,25 @@
 class Solution {
-    public List<Integer> luckyNumbers(int[][] m) {
-        ArrayList<Integer> minr=new ArrayList<>();
-        ArrayList<Integer> maxc=new ArrayList<>();
+    public List<Integer> luckyNumbers(int[][] arr) {
         ArrayList<Integer> res=new ArrayList<>();
-        
-        for(int r=0;r<m.length;r++){
-            int min=100000;
-            for(int c=0;c<m[0].length;c++){
-                if(min>m[r][c])
-                    min=m[r][c];
-            }
-            minr.add(min);
-        }
-        for(int r=0;r<m[0].length;r++){
+        int m=arr.length;
+        int n=arr[0].length;
+        for(int i=0;i<m;i++){
+            int min=arr[i][0];
             int max=0;
-            for(int c=0;c<m.length;c++){
-                if(max<m[c][r])
-                    max=m[c][r];
+            int colindex=0;
+            for(int col=0;col<n;col++){
+                if(arr[i][col]<min){
+                    min=arr[i][col];
+                    colindex=col;
+                }
             }
-            maxc.add(max);
-        }
-        for(int r=0;r<m.length;r++){
-            for(int c=0;c<m[0].length;c++){
-                // System.out.println(minr.get(r)+" "+maxc.get(c));
-                if(m[r][c]==minr.get(r) && m[r][c]==maxc.get(c))
-                    res.add(m[r][c]);
+            boolean istrue=true;
+            for(int col=0;col<m;col++){
+                if(arr[col][colindex]>min)
+                    istrue=false;
             }
+            if(istrue) res.add(min);
+
         }
         return res;
     }
