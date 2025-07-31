b1614f55e0a3f113bc6e9a038ce9cce6cc0b83ac
VISHNUMANIKANDAN T <155707903+VishnuMK2006@users.noreply.github.com>
2025-08-01 04:53:30 +0530

    Time: 1 ms (62.40%) | Memory: 41.9 MB (17.56%) - LeetSync

--- 1797-goal-parser-interpretation/goal-parser-interpretation.java
+++ 1797-goal-parser-interpretation/goal-parser-interpretation.java
@@ -1,16 +1,6 @@
 class Solution {
     public String interpret(String cmd) {
-        // String arr[]={"o","al","G"};
-        // int temp=0;
-        // StringBuilder res=new StringBuilder();
-        // for(int i=0;i<cmd.length();i++){
-        //     temp+=cmd.charAt(i);
-        //     if(temp%3<3){
-        //        res.append(arr[temp%3]);
-        //        temp=0;
-        //        }
-        // }
-        cmd=cmd.replace("()","o");
-        cmd=cmd.replace("(al)","al");
+       String s=cmd.replace("()","o");
+        cmd=s.replace("(al)","al");
         return cmd;
 }}
