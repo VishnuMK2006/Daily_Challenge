305b716daac713ae0331502e92978ed6f5cc7014
VISHNUMANIKANDAN T <155707903+VishnuMK2006@users.noreply.github.com>
2025-08-03 18:57:19 +0530

    Time: 1 ms (99.84%) | Memory: 44.4 MB (89.58%) - LeetSync

--- 1482-how-many-numbers-are-smaller-than-the-current-number/how-many-numbers-are-smaller-than-the-current-number.java
+++ 1482-how-many-numbers-are-smaller-than-the-current-number/how-many-numbers-are-smaller-than-the-current-number.java
@@ -1,13 +1,18 @@
 class Solution {
     public int[] smallerNumbersThanCurrent(int[] nums) {
         int res[]=new int[nums.length];
+        int fre[]=new int[101];
+        //take the feq of the array
         for(int i=0;i<nums.length;i++){
-            int count=0;
-           for(int j=0;j<nums.length;j++){
-                if(i!=j && nums[j]<nums[i])
-                    count++;
-        } 
-        res[i]=count;
+            fre[nums[i]]++;
+        }
+        //make the prefix sum
+        for(int i=1;i<101;i++){
+            fre[i]+=fre[i-1];
+        }
+        //res construct
+        for(int i=0;i<nums.length;i++){
+            res[i]= nums[i]==0? 0:fre[nums[i]-1];
         }
         return res;
     }
