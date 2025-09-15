496127f11d5de8f0ca11676008c35b7e2333bad4
VISHNUMANIKANDAN T <155707903+VishnuMK2006@users.noreply.github.com>
2025-09-15 14:27:46 +0530

    Time: 0 ms (100.00%) | Memory: 41.9 MB (83.43%) - LeetSync

--- 75-sort-colors/sort-colors.java
+++ 75-sort-colors/sort-colors.java
@@ -1,5 +1,22 @@
+//dutch flag algorithm.
+
 class Solution {
     public void sortColors(int[] nums) {
-        Arrays.sort(nums);
+        int l=0,r=nums.length-1;
+        for(int i=0;i<nums.length && i <=r;i++){
+            if(nums[i]==0){
+                int temp = nums[l];
+                nums[l++]=nums[i];
+                nums[i]=temp;
+                }
+            else if(nums[i]==1)
+                continue;
+            else{
+                int temp = nums[r];
+                nums[r--]=nums[i];
+                nums[i]=temp;
+                i--;
+                }
+        }
     }
 }
