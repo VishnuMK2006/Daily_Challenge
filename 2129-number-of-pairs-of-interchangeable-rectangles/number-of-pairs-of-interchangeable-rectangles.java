ac52bcd9481a6ff6c6915ff346cf51f0f29d6fcf
VISHNUMANIKANDAN T <155707903+VishnuMK2006@users.noreply.github.com>
2025-09-15 14:06:29 +0530

    Time: 27 ms (96.09%) | Memory: 78 MB (93.65%) - LeetSync

--- 2129-number-of-pairs-of-interchangeable-rectangles/number-of-pairs-of-interchangeable-rectangles.java
+++ 2129-number-of-pairs-of-interchangeable-rectangles/number-of-pairs-of-interchangeable-rectangles.java
@@ -1,19 +1,25 @@
 class Solution {
-    public double[] modify(int[][] nums){
-        double res[]=new double[nums.length];
-        for(int i=0;i<nums.length;i++){
-                res[i]=(double)nums[i][0]/nums[i][1];   
-        }
-        return res;
-    }
     public long interchangeableRectangles(int[][] rectangles) {
-        double arr[]=modify(rectangles);
-        HashMap<Double,Integer> hs=new HashMap();
-        long res=0;
-        for(int i=0;i<arr.length;i++){
-            res+=hs.getOrDefault(arr[i],0);
-            hs.put(arr[i], hs.getOrDefault(arr[i],0)+1);
+        double[] ratio = new double[rectangles.length];
+        int i=0;
+        for(int [] arr:rectangles){
+            ratio[i] = (double)arr[0]/arr[1];
+            i++;
         }
-        return res;
+        long ans = 0;
+        Arrays.sort(ratio);
+        double ch = ratio[0];
+        int cc = 1;
+        for(int j=1;j<ratio.length;j++){
+            if(ch == ratio[j]){
+                ans +=(long)cc;
+                cc+=1;
+            }
+            else{
+                ch = ratio[j];
+                cc = 1;
+            }
+        }
+        return ans;
     }
 }
