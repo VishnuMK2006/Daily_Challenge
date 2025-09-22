1bfde625fa66ff0fc95b304f28bc50e2dbcc2eb5
VISHNUMANIKANDAN T <155707903+VishnuMK2006@users.noreply.github.com>
2025-09-22 10:26:41 +0530

    Time: 0 ms (100.00%) | Memory: 40.4 MB (50.05%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.java
+++ 1013-fibonacci-number/fibonacci-number.java
@@ -1,9 +1,9 @@
-//dynamic programming.
-class Solution {
+//dynamic programming.---> top down approch
+/*class Solution {
     public int calfib(int n,int memo[]){
         if(memo[n]!=-1) return memo[n]; //check the values in the table , if return the valus
         if(n==0) return memo[n] = 0; //before returning the value store it in the respective index 
-        if(n==1) return memo[n] = 1; //   ''
+        if(n==1) return memo[n] = 1; //   ''              ''                 ''             ''
         return memo[n] = calfib(n-1,memo) + calfib(n-2,memo);
 
     }
@@ -11,5 +11,21 @@ class Solution {
         int[] arr=new int[n+1]; //this is the momoization table
         Arrays.fill(arr,-1); //initlize with -1 , becz the -1 is not a valid fibo
         return calfib( n, arr); //calculate the fibo values.
+    }
+}
+*/
+
+//dynamic programming.---> Bottom up approch
+
+
+class Solution {
+    public int fib(int n) {
+        int arr[] = new int[n+1];
+        if(n<2) return n;
+        arr[1]=1;
+        for(int i=2;i<=n;i++){
+            arr[i]=arr[i-1]+arr[i-2];
+        }
+        return arr[n];
     }
 }
