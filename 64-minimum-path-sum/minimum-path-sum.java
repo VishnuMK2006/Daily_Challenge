71dd034daac769dec240e591be014630e2723f8e
VISHNUMANIKANDAN T <155707903+VishnuMK2006@users.noreply.github.com>
2025-09-23 10:35:51 +0530

    Time: 4 ms (59.61%) | Memory: 47.5 MB (69.04%) - LeetSync

--- 64-minimum-path-sum/minimum-path-sum.java
+++ 64-minimum-path-sum/minimum-path-sum.java
@@ -1,3 +1,5 @@
+/*
+
 class Solution {
     public int minPathSum(int[][] grid,int r,int c,int memo[][]) {
         if(memo[r][c]!=-1) return memo[r][c];
@@ -12,5 +14,34 @@ class Solution {
             Arrays.fill(row, -1);
         }
         return minPathSum(grid,grid.length-1,grid[0].length-1,memo);
+    }
+}
+
+
+ */
+
+
+class Solution {
+    public int minPathSum(int[][] grid) {
+        int db[][] = new int[grid.length][grid[0].length];
+        db[0][0]=grid[0][0];
+
+        //only row
+        for(int i=1;i<grid[0].length;i++){
+            db[0][i] = grid[0][i]+ db[0][i-1];
+        }
+
+        //only col
+        for(int i=1;i<grid.length;i++){
+            db[i][0] = grid[i][0]+ db[i-1][0];
+        }
+
+        //full matrix
+        for(int r=1;r<grid.length;r++){
+            for(int c=1;c<grid[0].length;c++){
+                db[r][c]= grid[r][c]+ Math.min(db[r-1][c], db[r][c-1]);
+            }
+        }
+        return db[grid.length-1][grid[0].length-1];
     }
 }
