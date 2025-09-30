1de815abbaa30ffdc78fe22d30e355f4786ad3cd
VISHNUMANIKANDAN T <155707903+VishnuMK2006@users.noreply.github.com>
2025-09-30 12:01:47 +0530

    Time: 158 ms (15.89%) | Memory: 84.4 MB (22.31%) - LeetSync

---
+++ 234-palindrome-linked-list/palindrome-linked-list.py
@@ -0,0 +1,17 @@
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution(object):
+    def isPalindrome(self, head):
+        """
+        :type head: Optional[ListNode]
+        :rtype: bool
+        """
+        l=[]
+        while head is not None:
+            l.append(head.val)
+            head=head.next
+        return l== l[::-1]
+        
