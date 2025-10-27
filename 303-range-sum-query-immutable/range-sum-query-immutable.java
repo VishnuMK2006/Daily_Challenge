dfabe07efdbaed25b64df6191e029184bfd01e28
VISHNUMANIKANDAN T <155707903+VishnuMK2006@users.noreply.github.com>
2025-10-27 19:01:18 +0530

    Time: 8 ms (53.10%) | Memory: 49.9 MB (14.18%) - LeetSync

--- 303-range-sum-query-immutable/range-sum-query-immutable.java
+++ 303-range-sum-query-immutable/range-sum-query-immutable.java
@@ -3,7 +3,7 @@ class NumArray {
     public NumArray(int[] nums) {
        for(int i=1;i<nums.length;i++){
             nums[i]=nums[i-1]+nums[i];
-            System.out.print(nums[i]);
+            // System.out.print(nums[i]);
        }
        arr=nums;
     }
